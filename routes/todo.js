9bc470e1ab46b822f9bdb642560364524bc76056
aaronclare3 <aaronclare88@gmail.com>
2021-05-23 23:35:29 -0700

    added update, complete and create

--- routes/todo.js
+++ routes/todo.js
@@ -17,6 +17,47 @@ router.get("/:id", getTodo, (req, res) => {
   res.json(res.todo);
 });
 
+// Creating one
+router.post("/", async (req, res) => {
+  const todo = new Todo({
+    description: req.body.description,
+    isDone: req.body.isDone,
+  });
+  try {
+    const newTodo = await todo.save();
+    res.status(201).json(newTodo);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Updating One
+router.patch("/:id", getTodo, async (req, res) => {
+  if (req.body.description != null) {
+    res.todo.description = req.body.description;
+  }
+  if (req.body.isDone != null) {
+    res.todo.isDone = req.body.isDone;
+  }
+  try {
+    const updatedTodo = await res.todo.save();
+    res.json(updatedTodo);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Completing Todo
+router.patch("/complete/:id", getTodo, async (req, res) => {
+  res.todo.isDone = "true";
+  try {
+    const updatedTodo = await res.todo.save();
+    res.json(updatedTodo);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
 async function getTodo(req, res, next) {
   let todo;
   try {
