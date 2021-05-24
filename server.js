12c6355a5ec73dd8d1f23d6b0e810e2abefd39f6
aaronclare3 <aaronclare88@gmail.com>
2021-05-24 10:03:51 -0700

    changed wording

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ mongoose.connect(process.env.DATABASE_URL, {
 
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connected to Database"));
+db.once("open", () => console.log("Connected to MongoDB Database"));
 
 app.use(express.json());
 
 const todosRouter = require("./routes/todo");
 app.use("/todos", todosRouter);
 
-app.listen(3000, () => console.log("Server Started"));
+app.listen(3000, () => console.log("Server Started on port 3000!"));
